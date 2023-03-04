2b0b9e58e4535e0cefbedb23127796f5fee372f7
Deepankar-Mishra <49284074+Deepankar-Mishra@users.noreply.github.com>
2023-03-04 19:32:28 +0530

    train.py

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ from keras.datasets import fashion_mnist
 
 # Load the Fashion-MNIST dataset
 (train_image,train_label),(test_image,test_label)=fashion_mnist.load_data()
-
 # Define the class names
 class_type=['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker', 'Bag','Ankle boot']
 
@@ -18,3 +17,5 @@ for i in range(len(class_type)):
     axs[row, col].set_title(class_type[i])
     axs[row, col].axis('off')
 plt.show()
+train_image = train_image.reshape(train_image.shape[0], train_image.shape[1]*train_image.shape[2])
+train_label = train_label.reshape(train_label.shape[0],1)
